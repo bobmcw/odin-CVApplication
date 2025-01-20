7f80e2423fc6c43308463b0d5fed69a02cf7b7b8
bobmcw <jasiekz2014@gmail.com>
2025-01-20 18:09:18 +0100

    added sections to the preview

--- src/preview.jsx
+++ src/preview.jsx
@@ -1,7 +1,7 @@
 import './preview.css'
 function Preview(){
     return(
-        <>
+        <div className="preview">
         <header>
             <h1>Header</h1>
             <div className="headerItems">
@@ -10,7 +10,11 @@ function Preview(){
                 <p>item3</p>
             </div>
         </header>
-        </>
+        <div className="previewBody">
+            <h2>Education</h2>
+            <h2>Experience</h2>
+        </div>
+        </div>
     )
 }
 export default Preview;
