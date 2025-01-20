9526c7efbec7ca741b7e65f14d81ca5d6423645a
bobmcw <jasiekz2014@gmail.com>
2025-01-20 17:35:55 +0100

    created a sample preview component

---
+++ src/preview.jsx
@@ -0,0 +1,16 @@
+import './preview.css'
+function Preview(){
+    return(
+        <>
+        <header>
+            <h1>Header</h1>
+            <div className="headerItems">
+                <p>item1</p>
+                <p>item2</p>
+                <p>item3</p>
+            </div>
+        </header>
+        </>
+    )
+}
+export default Preview;
