6d53d52b46568072ab8db21e7667cef4bbfe2200
bobmcw <jasiekz2014@gmail.com>
2025-01-21 14:28:01 +0100

    removed unneeded div

--- src/panelSetting.jsx
+++ src/panelSetting.jsx
@@ -1,12 +1,10 @@
 import './panelSetting.css'
 function PanelSetting({name}){
     return(
-    <>
     <div className="settingContainer">
         <label htmlFor={name+"Input"}>{name}</label>
         <input type="text" id={name+"Input"} />
     </div>
-    </>
     )
 }
 export default PanelSetting;
